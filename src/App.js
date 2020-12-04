82d071221befdddc09687fd7e4d57d7e4ab4b80a
Shel <>
2020-12-04 16:28:04 -0600

    First grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import HexagonGrid from 'react-hexagon-grid';
+import times from 'lodash/times';
+
+function roll(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const generatePlots = (size) => {
+  let plots = [];
+  times(size, id => {
+      let dice = roll(100);
+      if (dice<=3){
+        plots[id] = { id: id, isFlower: true, name: "flower"};
+      } else {
+        plots[id] = {id: id, isFlower: false, name: "grass"};
+      }
+  });
+  return plots;
+}
+const HexGridDemo = () =>  {
+  const getHexProps = (hexagon) => {
+    let fill = "#007aff";
+    if (hexagon.isFlower){
+      fill = "red";
+    }
+    return {
+      style: {
+        fill: fill,
+        stroke: 'none'
+      },
+      onClick: () => alert(`Hexagon n.${hexagon.id} has been clicked`)
+    };
+  }
+
+  const renderHexagonContent = (hexagon) => {
+    return (
+      <text
+        x="50%"
+        y="50%"
+        fontSize={100}
+        fontWeight="lighter"
+        style={{ fill: 'white' }}
+        textAnchor="middle"
+      >
+        {hexagon.name}{hexagon.id}
+      </text>
+    );
+  }
+
+  let hexagons = generatePlots(102);
+
+  return (
+    <HexagonGrid
+    gridWidth={600}
+    gridHeight={600}
+    hexagons={hexagons}
+    hexProps={getHexProps}
+    renderHexagonContent={renderHexagonContent}
+    />
+  );
+}
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+      <HexGridDemo />
+      </div>
         <p>
-          Edit... wumba<code>src/App.js</code> and save to reload.
+          TINY COLLECTOR
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+    </>
   );
 }
 
---
+++ src/demo.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import HexagonGrid from 'react-hexagon-grid';
+import times from 'lodash/times';
+
+const HexGridDemo = () =>  {
+  const getHexProps = (hexagon) => {
+    return {
+      style: {
+        fill: '#007aff',
+        stroke: 'white'
+      },
+      onClick: () => alert(`Hexagon n.${hexagon} has been clicked`)
+    };
+  }
+
+  const renderHexagonContent = (hexagon) => {
+    return (
+      <text
+        x="50%"
+        y="50%"
+        fontSize={100}
+        fontWeight="lighter"
+        style={{ fill: 'white' }}
+        textAnchor="middle"
+      >
+        {hexagon}
+      </text>
+    );
+  }
+
+  let hexagons = times(102, id => id);
+
+  return (
+    <HexagonGrid
+    gridWidth={500}
+    gridHeight={500}
+    hexagons={hexagons}
+    hexProps={getHexProps}
+    renderHexagonContent={renderHexagonContent}
+    />
+  );
+}
+
+ReactDOM.render(
+  <HexGridDemo />,
+  document.getElementById('root')
+);
